a9549d67946458d06a5eacbf6c11133853b1cd30
jkread <jr@johnkread.com>
2021-02-18 23:55:02 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,18 +4,24 @@ echo "##########################################################################
 echo "# H265ize Server - " `date`
 echo "# Input: " $INPUT
 echo "# Output: " $OUTPUT
+echo "# Temp: " $TEMP
 echo "# Quality: " $QUALITY
 echo "# Preset: " $PRESET
 echo "# Overide: " $OVERIDE
 echo "# Delete: " $DELETE
+echo "# Replace: " $REPLACE
+echo "# Preview: " $PREVIEW
 echo "# Sleep: " $SLEEP
+echo "# Debug: " $DEBUG
 echo "###########################################################################"
 
 args=()
-args+=( "$INPUT" '-d' "$OUTPUT" )
+args+=( "$INPUT" '-d' "$OUTPUT" '-t' "$TEMP" )
 [ "$QUALITY" != "" ] && args+=( '-q' "$QUALITY" )
 [ "$OVERIDE" == "true" ] && args+=( '-o' )
 [ "$DELETE" == "true" ] && args+=( '--delete' )
+[ "$REPLACE" == "true" ] && args+=( '--replace' )
+[ "$PREVIEW" == "true" ] && args+=( '--preview' )
 [ "$ASPRESET" != "" ] && args+=( '--as-preset' "$ASPRESET" )
 [ "$PRESET" != "" ] && args+=( '-m' "$PRESET" )
 [ "$TUNE" != "" ] && args+=( '--tune' "$TUNE" )
