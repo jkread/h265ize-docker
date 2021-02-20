937ef4a2dd30a5dad6b1460d21eaa301dfb579fa
jkread <jr@johnkread.com>
2021-02-19 21:49:03 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 
 echo "###########################################################################"
-echo "# H265ize Server - " `date`
+echo "# H265ize Server" 
+echo "# " `date`
 echo "# Input: " $INPUT
 echo "# Output: " $OUTPUT
 echo "# Temp: " $TEMP
-echo "# Quality: " $QUALITY
-echo "# Preset: " $PRESET
-echo "# Overide: " $OVERIDE
-echo "# Delete: " $DELETE
-echo "# Replace: " $REPLACE
-echo "# Preview: " $PREVIEW
-echo "# Sleep: " $SLEEP
-echo "# Debug: " $DEBUG
-echo "###########################################################################"
-
 args=()
 args+=( "$INPUT" '-d' "$OUTPUT" '-t' "$TEMP" )
-[ "$QUALITY" != "" ] && args+=( '-q' "$QUALITY" )
-[ "$OVERIDE" == "true" ] && args+=( '-o' )
-[ "$DELETE" == "true" ] && args+=( '--delete' )
-[ "$REPLACE" == "true" ] && args+=( '--replace' )
-[ "$PREVIEW" == "true" ] && args+=( '--preview' )
-[ "$ASPRESET" != "" ] && args+=( '--as-preset' "$ASPRESET" )
-[ "$PRESET" != "" ] && args+=( '-m' "$PRESET" )
-[ "$TUNE" != "" ] && args+=( '--tune' "$TUNE" )
-[ "$REF" != "" ] && args+=( '--ref' "$REF" )
-[ "$WEIGHTB" == "true" ] && args+=( '--weightb' )
-[ "$BFRAMES" != "" ] && args+=( '--bframes' "$BFRAMES" )
-[ "$SCALE" != "" ] && args+=( '--scale' "$SCALE" )
-[ "$DEBUG" == "true" ] && args+=( '--debug' )
-[ "$STATSFREQUENCY" != "" ] && args+=( '--stats-frequency', "$STATSFREQUENCY" )
+[ "$QUALITY" != "" ] && args+=( '-q' "$QUALITY" ) && echo "# Quality: " $QUALITY
+[ "$PRESET" != "" ] && args+=( '-m' "$PRESET" ) && echo "# Preset: " $PRESET
+[ "$ASPRESET" != "" ] && args+=( '--as-preset' "$ASPRESET" ) && echo "# AS Preset: " $ASPRESET
+[ "$OVERIDE" == "true" ] && args+=( '-o' ) && echo "# Overide: " $OVERIDE
+[ "$DELETE" == "true" ] && args+=( '--delete' ) && echo "# Delete: " $DELETE
+[ "$REPLACE" == "true" ] && args+=( '--replace' ) && echo "# Replace: " $REPLACE
+[ "$PREVIEW" == "true" ] && args+=( '--preview' ) && echo "# Preview: " $PREVIEW
+[ "$TUNE" != "" ] && args+=( '--tune' "$TUNE" ) && echo "# Tune: " $TUNE
+[ "$REF" != "" ] && args+=( '--ref' "$REF" ) && echo "# Ref Frames: " $REF
+[ "$WEIGHTB" == "true" ] && args+=( '--weightb' ) && echo "# Weight B: " $WEIGHTB
+[ "$BFRAMES" != "" ] && args+=( '--bframes' "$BFRAMES" ) && echo "# B Frames: " $BFRAMES
+[ "$SCALE" != "" ] && args+=( '--scale' "$SCALE" ) && echo "# Scale Y: " $SCALE
+[ "$DEBUG" == "true" ] && args+=( '--debug' ) && echo "# DEBUG: " $DEBUG
+[ "$STATSFREQUENCY" != "" ] && args+=( '--stats-frequency' "$STATSFREQUENCY" ) && echo "# Stata Frequency: " "$STATSFREQUENCY"
+[ "$DIFFLIMIT" != "" ]&& args+=( '--diff-limit' "$DIFFLIMIT" ) && echo "# Diff Limit: " "$DIFFLIMIT"
+echo "# Sleep: " $SLEEP
+echo "###########################################################################"
 
 while :
 do
